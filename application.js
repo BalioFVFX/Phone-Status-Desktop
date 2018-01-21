e75f00842ff45f556c14d4c047af4d1904a9130a
Erik Baliov <vfxbaliof@gmail.com>
2018-01-21 15:15:34 +0200

    Switched to EmailAndPassword login

--- application.js
+++ application.js
@@ -1,52 +1,47 @@
-var config = {
-    apiKey: "AIzaSyBw8GMjN-xcDmCtDiO6LcK7gNwsjWWF0ak",
-    authDomain: "phone-status-desktop.firebaseapp.com",
-    databaseURL: "https://phone-status-desktop.firebaseio.com",
-    projectId: "phone-status-desktop",
-    storageBucket: "phone-status-desktop.appspot.com",
-    messagingSenderId: "404407908944"
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-
-
-  var starCountRef = firebase.database().ref('users/' + currentUsername + "/");
-  starCountRef.on('value', function(snapshot) {
-    var chargingStatusImage = document.getElementById('chargingStatusImage');
-
-   
-    if(document.getElementById('batteryLevel') != null){
-      document.getElementById('batteryLevel').innerHTML = "Battery Level: " + snapshot.val().batterystatus.level + "%";
-      if(snapshot.val().batterystatus.isCharging == true){
-        if(snapshot.val().batterystatus.acCharge == true){
-            document.getElementById('isCharging').innerHTML = "Battery Status: Charging - Ac Charger"
-            document.getElementById('chargingStatusImage').src = "plug.png";
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    var starCountRef = firebase.database().ref('users/' + user.uid + "/");
+    starCountRef.on('value', function(snapshot) {
+      var chargingStatusImage = document.getElementById('chargingStatusImage');
+  
+     
+      if(document.getElementById('batteryLevel') != null){
+        document.getElementById('batteryLevel').innerHTML = "Battery Level: " + snapshot.val().batterystatus.batterylevel + "%";
+        if(snapshot.val().batterystatus.isCharging == true){
+          if(snapshot.val().batterystatus.acCharge == true){
+              document.getElementById('isCharging').innerHTML = "Battery Status: Charging - Ac Charger"
+              document.getElementById('chargingStatusImage').src = "plug.png";
+              chargingStatusImage.style.width = '35px';
+              chargingStatusImage.style.height = '35px';
+              chargingStatusImage.style.left = '5px';
+  
+          }
+          else{
+            document.getElementById('isCharging').innerHTML = "Battery Status: Charging - USB"
+            document.getElementById('chargingStatusImage').src = "usb.png";
             chargingStatusImage.style.width = '35px';
             chargingStatusImage.style.height = '35px';
-            chargingStatusImage.style.left = '5px';
-
+            chargingStatusImage.style.left = '46px';
+          }
         }
         else{
-          document.getElementById('isCharging').innerHTML = "Battery Status: Charging - USB"
-          document.getElementById('chargingStatusImage').src = "usb.png";
-          chargingStatusImage.style.width = '35px';
-          chargingStatusImage.style.height = '35px';
-          chargingStatusImage.style.left = '46px';
+            document.getElementById('isCharging').innerHTML = "Battery Status: Discharging"
+            document.getElementById('chargingStatusImage').src = "//:0";
+            chargingStatusImage.style.width = '0px';
+            chargingStatusImage.style.height = '0px';
         }
       }
-      else{
-          document.getElementById('isCharging').innerHTML = "Battery Status: Discharging"
-          document.getElementById('chargingStatusImage').src = "//:0";
-          chargingStatusImage.style.width = '0px';
-          chargingStatusImage.style.height = '0px';
+     
+     
+      else if(document.getElementById('batteryTemperature') != null){
+        document.getElementById('batteryTemperature').innerHTML = "Battery Temperature: " + snapshot.val().batterystatus.temperature + "&#8451;";
       }
-    }
-   
-   
-    else if(document.getElementById('batteryTemperature') != null){
-      document.getElementById('batteryTemperature').innerHTML = "Battery Temperature: " + snapshot.val().batterystatus.temperature + "&#8451;";
-    }
-   
+     
+      
+    });
     
-  });
+  } else {
+    console.log('Error');
+  }
+});
+ 
--- login.js
+++ login.js
@@ -1,16 +1,5 @@
 function login(){
 
-      // Initialize Firebase
-      var config = {
-        apiKey: "AIzaSyBw8GMjN-xcDmCtDiO6LcK7gNwsjWWF0ak",
-        authDomain: "phone-status-desktop.firebaseapp.com",
-        databaseURL: "https://phone-status-desktop.firebaseio.com",
-        projectId: "phone-status-desktop",
-        storageBucket: "phone-status-desktop.appspot.com",
-        messagingSenderId: "404407908944"
-      };
-      firebase.initializeApp(config);
-
       var email = document.getElementById('email').value;
       var password = document.getElementById('password').value;
       
@@ -23,15 +12,16 @@ function login(){
         // ...
       }
   );
+  console.log(firebase.auth().currentUser.email)
+}
 
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      console.log('Success');
-      window.location.assign("batterylevel.html")
-    } else {
-      console.log('Error');
-    }
-  });
-       
-        
-      }
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log('success');
+   
+    
+    window.location.assign("batterylevel.html")
+  } else {
+    console.log('Error');
+  }
+});
