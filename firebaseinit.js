59d90e89794c813f9040ebdc52aa9051d61b1a5e
Erik Baliov <vfxbaliof@gmail.com>
2018-01-21 15:37:29 +0200

    Firebase init file

---
+++ firebaseinit.js
@@ -0,0 +1,19 @@
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyBw8GMjN-xcDmCtDiO6LcK7gNwsjWWF0ak",
+            authDomain: "phone-status-desktop.firebaseapp.com",
+            databaseURL: "https://phone-status-desktop.firebaseio.com",
+            projectId: "phone-status-desktop",
+            storageBucket: "phone-status-desktop.appspot.com",
+            messagingSenderId: "404407908944"
+          };
+  
+        firebase.initializeApp(config);
+        
+       
+
+  
+            
+
+           
+        
