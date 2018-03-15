120ba4420c2fafda5acef85ff3aa538c345e84b0
Erik Baliov <vfxbaliof@gmail.com>
2018-03-15 16:45:12 +0200

    Quit the application when the window is closed

--- main.js
+++ main.js
@@ -4,20 +4,18 @@ const url = require('url')
 
 let win
 
-function createWindow(){
 
+function createWindow(){
   // Create the browser window
 
   win = new BrowserWindow({width: 500, height: 645, title: "Phone Status"})
-
   // Load the url
-
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'login.html'),
     protocol: 'file:'
   }))
-}
 
+}
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
