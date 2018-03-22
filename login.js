0116fb317f0b589cf1ca77b064ab67327b69d111
Erik Baliov <vfxbaliof@gmail.com>
2018-03-22 18:55:21 +0200

    Showing error messages for wrong email or password

--- login.js
+++ login.js
@@ -8,8 +8,19 @@ function login(){
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorCode);
+        if(error.message == 'The email adress is badly formatted.'){
+          console.log('test');
+        }
         console.log(errorMessage);
-        // ...
+        if(errorMessage == 'The email address is badly formatted.'){
+          document.getElementById('email').style.color = 'red';
+          document.getElementById('email').value = '';
+          document.getElementById('email').placeholder = 'Email not found.';
+        }
+        else if(errorMessage == 'The password is invalid or the user does not have a password.'){
+          document.getElementById('password').value = '';
+          document.getElementById('password').placeholder = 'Invalid password';
+        }
       }
   );
   console.log(firebase.auth().currentUser.email)
@@ -22,6 +33,6 @@ firebase.auth().onAuthStateChanged(function(user) {
     
     window.location.assign("batterylevel.html")
   } else {
-    console.log('Error');
+    
   }
 });
